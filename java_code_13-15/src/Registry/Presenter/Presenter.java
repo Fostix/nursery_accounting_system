77d53e1f96c810aead2960c3627232920152c7a2
Albert <accretiondiskthat@gmail.com>
2023-02-02 17:14:22 +0500

    error: Your local changes to the following files would be overwritten by merge: 	java_code_13-15/out/production/java_code_13-15/Registry/Model/Creator.class 	java_code_13-15/out/production/java_code_13-15/Registry/Model/Model.class 	java_code_13-15/out/production/java_code_13-15/Registry/Presenter/IModel.class 	java_code_13-15/out/production/java_code_13-15/Registry/Presenter/Presenter.class 	java_code_13-15/src/Registry/Model/FriendsOfMan/Connector/Query.java 	java_code_13-15/src/Registry/Model/Model.java 	java_code_13-15/src/Registry/Presenter/IModel.java 	java_code_13-15/src/Registry/Presenter/Presenter.java Please commit your changes or stash them before you merge. error: The following untracked working tree files would be overwritten by merge: 	java_code_13-15/out/test/java_code_13-15/Registry/Model/ModelTest.class 	java_code_13-15/test/Registry/Model/ModelTest.java Please move or remove them before you merge. Aborting

--- java_code_13-15/src/Registry/Presenter/Presenter.java
+++ java_code_13-15/src/Registry/Presenter/Presenter.java
@@ -8,9 +8,11 @@ public class Presenter {
     private ViewContract viewContract;
     private final IModel model;
     private ArrayList<Animal> tempPets;
+    private static String[] animalType;
 
     public Presenter(IModel model) {
         this.model = model;
+        animalType = new String[]{"hamsters", "cats", "dogs", "horses", "donkeys"}; // "camels" table were delete.
     }
     public void attachView(ViewContract viewContract) {
         this.viewContract = viewContract;
@@ -22,28 +24,62 @@ public class Presenter {
 
     public void menu() {
         viewContract.showMenu();
-        String num = "1"; // viewContract.enterData();
+        // viewContract.enterData();
+        String num = "3";
         switch (num) {
             case "1":
-                ArrayList<Animal> listAnimal = model.getListOfAllPets("dogs");
-                for (Animal animal : listAnimal) {
-                    viewContract.print(Integer.toString(animal.getId()));
-                    viewContract.print(animal.getDateOfBirth());
-                    viewContract.println(animal.getName());
-                }
+                showAllPets();
+                break;
+            case "2":
+                searchPetById();
+                break;
+            case "3":
+                searchPetByDateOfBirth();
+                break;
+            case "4":
+                searchPetByName();
+                break;
+            case "5":
+                addNewPet();
+                break;
         }
     }
 
     public void showAllPets() {
-
+        viewContract.println("pets");
+        for (int i = 0; i < animalType.length; i++) {
+            ArrayList<Animal> listAnimal = model.getListOfAllPets(animalType[i]);
+            if (i == 3) // after 3 show pack animals
+                viewContract.println("pack animals");
+            for (Animal animal : listAnimal) {
+                viewContract.print(Integer.toString(animal.getId()));
+                viewContract.print(animal.getDateOfBirth());
+                viewContract.println(animal.getName());
+            }
+        }
     }
 
     public void searchPetById() {
-
+        viewContract.print("enter pet table: ");
+        String value = viewContract.enterData();
+        viewContract.print("enter pet id: ");
+        String id = viewContract.enterData();
+        Animal animal = model.getPetById(value, Integer.parseInt(id));
+        if (animal == null) {
+            viewContract.println("wrong");
+        } else {
+            viewContract.print(Integer.toString(animal.getId()));
+            viewContract.print(animal.getDateOfBirth());
+            viewContract.println(animal.getName());
+        }
     }
 
     public void searchPetByDateOfBirth() {
-
+        viewContract.print("Enter date of birth");
+        String data = viewContract.enterData();
+        for (String at : animalType) {
+            model.getPetByDataOfBirth(at, data);
+        }
     }
 
     public void searchPetByName() {
@@ -59,6 +95,24 @@ public class Presenter {
     }
 
     public void addNewPet() {
+        viewContract.print("Enter type of pet");
+        String type = viewContract.enterData();
+        typePetInId(type);
+        viewContract.print("Enter date of birth");
+        String dateOfBirth = viewContract.enterData();
+        viewContract.print("Enter name");
+        String data = viewContract.enterData();
+        model.addNewPet(type, typePetInId(type), dateOfBirth, data);
+    }
 
+    public int typePetInId(String type) {
+        for (int i = 0; i < animalType.length; i++) {
+            if (animalType[i].equals(type)) {
+                if (i < 2) {
+                    return 1;
+                } else return 2;
+            }
+        }
+        return -1;
     }
 }
